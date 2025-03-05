17bc596bf2ab87ae38a7ad722faa0e022ff2bf8b
irlmonster <heltsallad@hotmail.com>
2025-03-05 13:23:28 +0100

    Lagt till klasser Gjort om projektet till FX fixat POM.XML Fixat Hib.cfg.xml Lagt in gitignore

--- src/main/java/com/grp5/Main.java
+++ src/main/java/com/grp5/Main.java
@@ -6,20 +6,61 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.sql.*;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Label label = new Label("Välkommen till Hotell Wigell!");
+        Label label = new Label("Välkommen till Wigell Concerts!");
         StackPane root = new StackPane(label);
         Scene scene = new Scene(root, 400, 300);
 
-        primaryStage.setTitle("Hotell Wigell");
+        primaryStage.setTitle("Wigell Concerts");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
     public static void main(String[] args) {
+
+
+        String url =  "jdbc:mysql://localhost:3306/WigellConsertsDB";
+        String username = "root"; // användarnamn till databasen
+        String password = "root";   // lösenord till databasen
+
+        try {
+            // Steg 1: skapa anslutning
+            Connection connection = DriverManager.getConnection(url, username, password);
+
+            // Steg 2: skapa ett förberett uttalande med en paramter
+            String sql = "SELECT * FROM WigellConsertsDB WHERE id = ?"; //  "?" ersätts av värdet från applikationen
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            //Ange värdet för parametern
+            int userId = 123;
+            statement.setInt(1, userId); // om vi har flera "?" så väljer den ? på index 1
+
+            //Steg 3: Utför frågan och hämta resultatet
+            ResultSet resultSet = statement.executeQuery();  // vi hämtar alla (*) värden där userId är 123
+
+            //Steg 4: Bearbeta resultatet (logiken)
+            while (resultSet.next()) {
+                int  id = resultSet.getInt("userId"); // namnet på ett table
+                String name = resultSet.getString("name");
+                // hantera resten av resultatet här
+                System.out.println("AnvändarID: " + id + ", Namn: " + name);
+            }
+
+            //Steg 5: stäng resurser
+            resultSet.close();
+            statement.close();
+            connection.close();
+
+        }   catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+
         launch(args); // Startar JavaFX-applikationen
     }
 }
