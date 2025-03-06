3ac60aba33f45591dde670ce4c99e8cda2829aba
irlmonster <heltsallad@hotmail.com>
2025-03-06 06:40:30 +0100

    Lagt till getter och setters i WC
    Även @Entity, @Id och @GeneratedValue(strategy = GenerationType.IDENTITY) på alla klasser
    
    Det hade råkat försvinna

--- src/main/java/com/grp5/entitys/Arena.java
+++ src/main/java/com/grp5/entitys/Arena.java
@@ -1,6 +1,15 @@
 package com.grp5.entitys;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Arena {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String address;
--- src/main/java/com/grp5/entitys/WC.java
+++ src/main/java/com/grp5/entitys/WC.java
@@ -1,13 +1,62 @@
 package com.grp5.entitys;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import java.util.List;
 
+import java.util.List;
+
+@Entity
 public class WC {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private List<Concerts> conserts;
     private List<Customer> customers;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Concerts> getConserts() {
+        return conserts;
+    }
+
+    public void setConserts(List<Concerts> conserts) {
+        this.conserts = conserts;
+    }
+
+    public List<Customer> getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(List<Customer> customers) {
+        this.customers = customers;
+    }
+
+    @Override
+    public String toString() {
+        return "WC{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", conserts=" + conserts +
+                ", customers=" + customers +
+                '}';
+    }
 }
