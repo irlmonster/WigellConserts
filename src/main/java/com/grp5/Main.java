3d61b41ed5e5f6cc51ebcf6aeb7afe35df2d39ad
Simon-n-o <Simpan1998@hotmail.com>
2025-03-07 13:22:07 +0100

    Uppdaterat ArenaDAO

--- src/main/java/com/grp5/Main.java
+++ src/main/java/com/grp5/Main.java
@@ -1,6 +1,5 @@
 package com.grp5;
 
-import com.grp5.javaFX.FxManager;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -13,10 +12,18 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        new FxManager(primaryStage);
+        Label label = new Label("Välkommen till Wigell Concerts!");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 400, 300);
+
+        primaryStage.setTitle("Wigell Concerts");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
+
+
         String url =  "jdbc:mysql://localhost:3306/wigellconcertsdb";
         String username = "root"; // användarnamn till databasen
         String password = "root";   // lösenord till databasen
@@ -26,19 +33,19 @@ public class Main extends Application {
             Connection connection = DriverManager.getConnection(url, username, password);
             System.out.println("Anslutningen till databasen lyckades!");
             // Steg 2: skapa ett förberett uttalande med en paramter
-            String sql = "SELECT * FROM arena WHERE id = ?"; //  "?" ersätts av värdet från applikationen
+            String sql = "SELECT * FROM WigellConsertsDB WHERE id = ?"; //  "?" ersätts av värdet från applikationen
             PreparedStatement statement = connection.prepareStatement(sql);
 
             //Ange värdet för parametern
-            int address_id = 3;
-            statement.setInt(1, address_id); // om vi har flera "?" så väljer den ? på index 1
+            int userId = 123;
+            statement.setInt(1, userId); // om vi har flera "?" så väljer den ? på index 1
 
             //Steg 3: Utför frågan och hämta resultatet
             ResultSet resultSet = statement.executeQuery();  // vi hämtar alla (*) värden där userId är 123
 
             //Steg 4: Bearbeta resultatet (logiken)
             while (resultSet.next()) {
-                int  id = resultSet.getInt("address_id"); // namnet på ett table
+                int  id = resultSet.getInt("userId"); // namnet på ett table
                 String name = resultSet.getString("name");
                 // hantera resten av resultatet här
                 System.out.println("AnvändarID: " + id + ", Namn: " + name);
