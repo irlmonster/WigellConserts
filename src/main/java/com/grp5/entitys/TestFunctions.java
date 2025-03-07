b73e9d149f16398dc5d80b433b5205465fe3b474
Simon-n-o <Simpan1998@hotmail.com>
2025-03-07 13:51:10 +0100

    Lagt till metod för att lägga till namn och adress till databas

---
+++ src/main/java/com/grp5/entitys/TestFunctions.java
@@ -0,0 +1,37 @@
+package com.grp5.entitys;
+
+
+import java.sql.*;
+
+public class TestFunctions {
+    private static final String URL = "jdbc:mysql://localhost:3306/WigellConcertsDB";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Root";
+
+    public static void printAllCustomers() {
+        String sql = "SELECT * FROM customers";
+
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(sql)) {
+
+            System.out.println("Lista över alla kunder:");
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String firstName = rs.getString("first_name");
+                String lastName = rs.getString("last_name");
+                String birthDate = rs.getString("date_of_birth");
+                String phone = rs.getString("phone_number");
+                int addressId = rs.getInt("address_id");
+
+                System.out.println("ID: " + id + ", Namn: " + firstName + " " + lastName +
+                        ", Född: " + birthDate + ", Telefon: " + phone +
+                        ", AdressID: " + addressId);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
