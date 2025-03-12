16cb6828d08f754f56cc7d9b6a6382435f939da6
Simon-n-o <Simpan1998@hotmail.com>
2025-03-12 10:33:09 +0100

    Justerat så att registreringen kontrollerar om adressen redan finns.

--- src/main/java/com/grp5/entitys/CustomerManager.java
+++ src/main/java/com/grp5/entitys/CustomerManager.java
@@ -9,15 +9,25 @@ public class CustomerManager {
     public void registerUser(String firstName, String lastName, String birthDate, String phoneNumber,
                              String street, String houseNumber, String postalCode, String city) {
         try {
-            Addresses address = new Addresses();
-            address.setStreet(street);
-            address.setHouse_number(houseNumber);
-            address.setPostal_code(postalCode);
-            address.setCity(city);
+
 
             AddressDAO addressDAO = new AddressDAO();
-            addressDAO.saveAddress(address);
 
+            //kontrollerar om adressen redan är registrerad
+            Addresses address = addressDAO.findAddress(street, houseNumber, postalCode, city);
+
+            // om adressen inte finns lägger vi till den i databasen
+            if (address == null) {
+                address = new Addresses();
+                address.setStreet(street);
+                address.setHouse_number(houseNumber);
+                address.setPostal_code(postalCode);
+                address.setCity(city);
+
+                addressDAO.saveAddress(address);
+            }
+
+            //skapar och sparar kund
             Customer customer = new Customer();
             customer.setFirstName(firstName);
             customer.setLastName(lastName);
@@ -28,76 +38,13 @@ public class CustomerManager {
             CustomerDAO customerDAO = new CustomerDAO();
             customerDAO.saveCustomer(customer);
 
+
             TestFunctions.printAllCustomers();
+
 
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("Fel vid registrering av användare.");
         }
     }
-
-
-
-    /*
-
-    private static final String URL = "jdbc:mysql://localhost:3306/WigellConcertsDB";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Root";
-
-
-
-    public static void regUser(
-            String firstname, String lastname, String birthdate, String phone,
-            String street,String houseNumber, String city, String postalCode) {
-
-        String addressSQL = "INSERT INTO addresses (street, house_number, postal_code, city) VALUES (?, ?, ?, ?)";
-        String userSQL = "INSERT INTO customers (first_name, last_name, date_of_birth, phone_number, address_id) VALUES (?, ?, ?, ?, ?)";
-
-            // Steg 1: skapa anslutning
-        try { Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
-            System.out.println("Anslutningen till databasen lyckades!");
-
-                //start upp så att allt kan laddas in i databasen samtidigt
-                connection.setAutoCommit(false);
-
-
-            //lägger till adressen
-            int addressId = -1;
-            try (PreparedStatement addressStmt = connection.prepareStatement(addressSQL, Statement.RETURN_GENERATED_KEYS)) {
-                addressStmt.setString(1, street);
-                addressStmt.setString(2, houseNumber);
-                addressStmt.setString(3, city);
-                addressStmt.setString(4, postalCode);
-                addressStmt.executeUpdate();
-
-                try (ResultSet generatedKeys = addressStmt.getGeneratedKeys()) {
-                    if (generatedKeys.next()) {
-                        addressId = generatedKeys.getInt(1);
-                    }
-                }
-            }
-
-            if (addressId == -1) {
-                throw new SQLException("Misslyckades med att skapa adress, inget ID returnerades.");
-            }
-
-
-            //lägger till användare kopplad till adressen
-            try (PreparedStatement userStmt = connection.prepareStatement(userSQL)) {
-                userStmt.setString(1, firstname);
-                userStmt.setString(2, lastname);
-                userStmt.setString(3, birthdate);
-                userStmt.setString(4, phone);
-                userStmt.setInt(5, addressId);
-                userStmt.executeUpdate();
-            }
-
-            //lägger till allt till databasen
-            connection.commit();
-            System.out.println("Användare och adress registrerade!");
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }*/
-    }
+}
